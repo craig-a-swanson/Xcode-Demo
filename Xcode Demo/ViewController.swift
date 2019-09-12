73809852c461bd3d2a02d6ac9d0a0f3adead593c
Craig Swanson <cswanson02@me.com>
2019-09-11 21:39:31 -0500

    Initial Commit

---
+++ Xcode Demo/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Xcode Demo
+//
+//  Created by Craig Swanson on 9/11/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+
+// command+option+shift and click on file to choose where to open that file
+// command+/ :: comment/uncomment a line of code
+// control+t: transpose characters
+// control+command+e and click a variable to highlight all instances of that variable in your code
+// option+arrow: jumps to the end of the following word
+// control+i: re-indents everything
+// command+[]: move indentation left/right
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
